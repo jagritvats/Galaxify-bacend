846065e31a517edd398f1b17a0c61ed5add6c828
Inderpreet Singh1 <inder192002@gmail.com>
2023-10-08 04:11:17 +0530

    destination done

--- routes/destination.js
+++ routes/destination.js
@@ -3,21 +3,41 @@ const {capitalize} = require("../helpers/index.js")
 const router = require("express").Router();
 
 
+
 router.get('/all', async (req, res) => {
 
-    const {data, error} = await supabase
+    const {data: destinations, error} = await supabase
         .from('destinations')
-        .select()
-    res.send(data);
+        .select("*")
+
+    res.send(destinations);
 })
 
 router.get('/:name', async (req, res) => {
 
-    const {data, error} = await supabase
+    const {data: destination, error} = await supabase
         .from('destinations')
         .select()
         .eq("name", capitalize(req.params.name))
-    res.send(data);
+
+    if(error){
+        res.send({error: error})
+    }
+    
+    const {data: activities} = await supabase
+        .from('activities')
+        .select()
+        .in('id', destination[0].activities)
+
+    const {data: places} = await supabase
+        .from('places')
+        .select()
+        .in('id', destination[0].places)
+
+    destination[0].activities = activities
+    destination[0].places = places
+
+    res.send(destination[0]);
 })
 
 
