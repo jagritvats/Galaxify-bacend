7912794f50e5057df5302d2e5b13109d7aec46d1
Jagrit Vats <69034224+jagritvats@users.noreply.github.com>
2023-10-08 18:42:52 +0530

    switched to axios

--- service/openai.js
+++ service/openai.js
@@ -1,15 +1,24 @@
-const OpenAI = require('openai');
-const openai = new OpenAI();
+const axios = require('axios');
 
 const generatePrompt = async (text) => {
-	const chatCompletion = await openai.chat.completions.create({
-		messages: [{ role: 'user', content: text }],
-		model: 'gpt-3.5-turbo',
-	});
+	const response = await axios.post(
+		'https://api.openai.com/v1/chat/completions',
+		{
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: text }],
+		},
+		{
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+				// 'OpenAI-Organization': 'org-5UwEPtqb11fdilQkhqRuGKUh',
+			},
+		}
+	);
 
-	console.log(chatCompletion.choices);
+	console.log(response.data.choices);
 
-	return chatCompletion.choices[0].message.content;
+	return response.data.choices[0].message.content;
 };
 
 module.exports = generatePrompt;
