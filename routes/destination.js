43d5a29116872f233406d98579edbf2cf8f1ab05
Inderpreet Singh1 <inder192002@gmail.com>
2023-10-08 06:55:04 +0530

    null point handled

--- routes/destination.js
+++ routes/destination.js
@@ -1,47 +1,56 @@
-const supabase = require("../db/index.js")
-const {capitalize} = require("../helpers/index.js")
+const supabase = require("../db/index.js");
+const { capitalize } = require("../helpers/index.js");
 const router = require("express").Router();
 
+router.get("/all", async (req, res) => {
+  const { data: destinations, error } = await supabase
+    .from("destinations")
+    .select("*");
 
+  res.send(destinations);
+});
 
-router.get('/all', async (req, res) => {
+router.get("/:name", async (req, res) => {
+  const { data: destination, error } = await supabase
+    .from("destinations")
+    .select()
+    .eq("name", capitalize(req.params.name));
 
-    const {data: destinations, error} = await supabase
-        .from('destinations')
-        .select("*")
+  if (error) {
+    res.send({ error: error });
+  }
+  if (!!destination[0].activities) {
+    const { data: activities } = await supabase
+      .from("activities")
+      .select()
+      .in("id", destination[0].activities);
 
-    res.send(destinations);
-})
+    destination[0].activities = activities;
+  } else {
+    destination[0].activities = [];
+  }
 
-router.get('/:name', async (req, res) => {
+  if (!!destination[0].places) {
+    const { data: places } = await supabase
+      .from("places")
+      .select()
+      .in("id", destination[0].places);
 
-    const {data: destination, error} = await supabase
-        .from('destinations')
-        .select()
-        .eq("name", capitalize(req.params.name))
+    destination[0].places = places;
+  } else {
+    destination[0].places = [];
+  }
 
-    if(error){
-        res.send({error: error})
-    }
-    
-    const {data: activities} = await supabase
-        .from('activities')
-        .select()
-        .in('id', destination[0].activities)
+  if (!!destination[0].length) {
+    const { data: packages } = await supabase
+      .from("packages")
+      .select()
+      .in("id", destination[0].packages);
 
-    if(!!destination[0].places){
-        const {data: places} = await supabase
-            .from('places')
-            .select()
-            .in('id', destination[0].places)
-        
-        destination[0].places = places
-    }
+    destination[0].packages = packages;
+  }
 
-    destination[0].activities = activities
+  res.send(destination[0]);
+});
 
-    res.send(destination[0]);
-})
-
-
-module.exports = router
+module.exports = router;
