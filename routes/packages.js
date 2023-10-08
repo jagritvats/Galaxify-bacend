b1cb91023ae943b79898b281e4e7c0073a1e7964
Inderpreet Singh1 <inder192002@gmail.com>
2023-10-08 13:08:36 +0530

    pkg creation done

--- routes/packages.js
+++ routes/packages.js
@@ -1,30 +1,50 @@
-const Router = require('express').Router;
-const supabase = require('../db/index.js');
+const Router = require("express").Router;
+const supabase = require("../db/index.js");
 
 const router = Router();
 
-router.get('/all', async (req, res) => {
-	const { data, error } = await supabase.from('pkgs').select();
-	res.send(data);
+router.get("/all", async (req, res) => {
+  const { data, error } = await supabase.from("packages").select();
+  res.send(data);
 });
 
-router.get('/:id', async (req, res) => {
-	const { data: package, error } = await supabase
-		.from('packages')
-		.select()
-		.eq('id', parseInt(req.params.id));
+router.post("/create/new", async (req, res) => {
+  var id = new Date().getUTCMilliseconds();
+  const obj = {
+    id: id,
+    PackageName: req.body.PackageName,
+    Duration: req.body.Duration,
+    ActivityIDs: req.body.ActivityIDs,
+    Destination: req.body.Destination,
+    ImageUrl: "https://th.bing.com/th/id/OIG._ycLpE.beQuvaF_VjwLt?pid=ImgGn",
+  };
+  const { data, error } = await supabase.from("packages").insert(obj).select();
 
-	if (!!package[0].ActivityIDs) {
-		const { data: activities } = await supabase
-			.from("activities")
-			.select()
-			.in("id", package[0].ActivityIDs);
-	
-		package[0].activities = activities;
-	} else {
-		package[0].activities = [];
-	}
-	res.send(package);
+  if (error) {
+    res.send(error);
+    return;
+  }
+
+  res.send(data);
+});
+
+router.get("/:id", async (req, res) => {
+  const { data: package, error } = await supabase
+    .from("packages")
+    .select()
+    .eq("id", parseInt(req.params.id));
+
+  if (!!package[0].ActivityIDs) {
+    const { data: activities } = await supabase
+      .from("activities")
+      .select()
+      .in("id", package[0].ActivityIDs);
+
+    package[0].activities = activities;
+  } else {
+    package[0].activities = [];
+  }
+  res.send(package);
 });
 
 module.exports = router;
