f2b932b49e8fad5ec61c3ed0fc779cf347c5f6fb
Inderpreet Singh1 <inder192002@gmail.com>
2023-10-08 08:28:17 +0530

    package api done

--- routes/packages.js
+++ routes/packages.js
@@ -9,11 +9,22 @@ router.get('/all', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-	const { data, error } = await supabase
-		.from('pkgs')
+	const { data: package, error } = await supabase
+		.from('packages')
 		.select()
 		.eq('id', parseInt(req.params.id));
-	res.send(data);
+
+	if (!!package[0].ActivityIDs) {
+		const { data: activities } = await supabase
+			.from("activities")
+			.select()
+			.in("id", package[0].ActivityIDs);
+	
+		package[0].activities = activities;
+	} else {
+		package[0].activities = [];
+	}
+	res.send(package);
 });
 
 module.exports = router;
