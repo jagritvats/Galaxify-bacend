5db6cdb3f6e42a051e769cf16eb8ef159172feba
Jagrit Vats <69034224+jagritvats@users.noreply.github.com>
2023-10-08 18:17:46 +0530

    Revert "added openai's api"

--- routes/personalize.js
+++ routes/personalize.js
@@ -1,6 +1,4 @@
 const Router = require('express').Router;
-const createPersonalizedSummaryForUserSinglePlanet = require('../helpers/openai.js');
-const generatePrompt = require('../service/openai.js');
 const recommenderService = require('../service/recommender.js');
 
 const router = Router();
@@ -9,40 +7,7 @@ router.post('/', async (req, res) => {
 	const userPrefs = req.body;
 	console.log(req.body);
 	const itinerary = await recommenderService(userPrefs);
-	var resp = await generatePrompt(
-		`User with prefs : ${JSON.stringify(
-			userPrefs
-		)} has itinerary list: ${JSON.stringify(
-			itinerary
-		)}, select and return in only the (0-indexing) index of the best one like so : "x", without any text`
-	);
-	console.log(resp);
-	try {
-		let itineraryResult = itinerary[parseInt(resp)];
-		console.log(itineraryResult);
-		itineraryResult.aiSummary =
-			await createPersonalizedSummaryForUserSinglePlanet(
-				userPrefs,
-				itineraryResult.name
-			);
-		return res.json([itineraryResult]);
-	} catch (e) {
-		console.log('Invalid response from openai');
-	}
-	return res.json(itinerary);
-});
-
-router.post('/summary', async (req, res) => {
-	const userPrefs = req.body.userPrefs;
-	const planet = req.body.planet;
-	console.log(req.body);
-	const summaryForUser = await createPersonalizedSummaryForUserSinglePlanet(
-		userPrefs,
-		planet
-	);
-	res.json({
-		summary: summaryForUser,
-	});
+	res.json(itinerary);
 });
 
 module.exports = router;
