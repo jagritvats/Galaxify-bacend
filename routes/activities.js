59cd61d8c8b6f08bfa9292c3170e1ad73c372182
Inderpreet Singh1 <inder192002@gmail.com>
2023-10-08 11:38:27 +0530

    getting planets on activity index

--- routes/activities.js
+++ routes/activities.js
@@ -1,20 +1,28 @@
-const Router = require('express').Router;
-const supabase = require('../db/index.js');
-const { capitalize } = require('../helpers/index.js');
+const Router = require("express").Router;
+const supabase = require("../db/index.js");
+const { capitalize } = require("../helpers/index.js");
 
 const router = Router();
 
-router.get('/all', async (req, res) => {
-	const { data, error } = await supabase.from('activities').select();
-	res.send(data);
+router.get("/all", async (req, res) => {
+  const { data, error } = await supabase.from("activities").select();
+  res.send(data);
 });
 
-router.get('/:name', async (req, res) => {
-	const { data, error } = await supabase
-		.from('activities')
-		.select()
-		.eq('activityName', capitalize(req.params.name));
-	res.send(data);
+router.post("/planet", async (req, res) => {
+  const { data, error } = await supabase
+    .from("activities")
+    .select()
+    .in("id", req.body.activities);
+  res.send(data);
+});
+
+router.get("/:name", async (req, res) => {
+  const { data, error } = await supabase
+    .from("activities")
+    .select()
+    .eq("activityName", capitalize(req.params.name));
+  res.send(data);
 });
 
 module.exports = router;
