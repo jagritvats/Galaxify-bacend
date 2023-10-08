290971b6febb9403b0253fab03fc02fff6ce095c
Jagrit Vats <69034224+jagritvats@users.noreply.github.com>
2023-10-08 09:00:23 +0530

    added openai's api

--- routes/personalize.js
+++ routes/personalize.js
@@ -1,4 +1,5 @@
 const Router = require('express').Router;
+const createPersonalizedSummaryForUserSinglePlanet = require('../helpers/openai.js');
 const recommenderService = require('../service/recommender.js');
 
 const router = Router();
@@ -8,6 +9,19 @@ router.post('/', async (req, res) => {
 	console.log(req.body);
 	const itinerary = await recommenderService(userPrefs);
 	res.json(itinerary);
+});
+
+router.post('/summary', async (req, res) => {
+	const userPrefs = req.body.userPrefs;
+	const planet = req.body.planet;
+	console.log(req.body);
+	const summaryForUser = await createPersonalizedSummaryForUserSinglePlanet(
+		userPrefs,
+		planet
+	);
+	res.json({
+		summary: summaryForUser,
+	});
 });
 
 module.exports = router;
